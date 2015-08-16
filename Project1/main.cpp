43b340e19892d5f11703341ff1d32c81a15f8614
James Felts <jfelts1@users.noreply.github.com>
2015-08-16 04:47:50 -0700

    basic events are figured out

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -4,8 +4,12 @@
 
 int main(int argc, char **argv)
 {
-	ALLEGRO_DISPLAY* display = NULL;
-	ALLEGRO_BITMAP* testImage = NULL;
+	ALLEGRO_DISPLAY* display = nullptr;
+	ALLEGRO_BITMAP* testImage = nullptr;
+	ALLEGRO_EVENT_QUEUE* events = nullptr;
+	ALLEGRO_MOUSE_EVENT mouseEvent;
+	ALLEGRO_EVENT_SOURCE* mouseEventSource = nullptr;
+	ALLEGRO_EVENT mouseClick;
 
 	if (!al_init()) {
 		fprintf(stderr, "failed to initialize allegro!\n");
@@ -13,7 +17,7 @@ int main(int argc, char **argv)
 	}
 
 	display = al_create_display(640, 480);
-	if (!display) {
+	if (display == nullptr) {
 		fprintf(stderr, "failed to create display!\n");
 		return EXIT_FAILURE;
 	}
@@ -24,18 +28,52 @@ int main(int argc, char **argv)
 	}
 
 	testImage = al_load_bitmap("Images/test.bmp");
-	if (!testImage) {
+	if (testImage == nullptr) {
 		fprintf(stderr, "failed to load image\n");
 		return EXIT_FAILURE;
 	}
 
-	al_draw_bitmap(testImage, 0, 0,0);
+	events = al_create_event_queue();
+	if (events == nullptr) {
+		fprintf(stderr, "failed to create event queue\n");
+		return EXIT_FAILURE;
+	}
 
-	al_flip_display();
+	al_clear_to_color(al_map_rgb(0, 0, 0));
 
-	al_rest(10.0);
+	if (al_install_mouse()) {
+		mouseEventSource = al_get_mouse_event_source();
+		al_register_event_source(events, mouseEventSource);
+		while (true)
+		{
+			while (!al_is_event_queue_empty(events))
+			{	
+				al_get_next_event(events, &mouseClick);
+				ALLEGRO_EVENT_TYPE ty = mouseClick.type;
+				if (ty == ALLEGRO_EVENT_MOUSE_BUTTON_DOWN)
+				{
+					mouseEvent = mouseClick.mouse;
+					if (mouseEvent.button & 1)
+					{
+						printf("mouse click at x: %d y: %d\n", mouseEvent.x, mouseEvent.y);
+						al_draw_bitmap(testImage, mouseEvent.x, mouseEvent.y, 0);
+						al_flip_display();
+					}
+				}
+			}
+		}
+		
+		
+	}
+	else {
+		fprintf(stderr, "no mouse found!\n");
+		return EXIT_FAILURE;
+	}
 
+	al_rest(5);
 	al_destroy_display(display);
+
+	al_destroy_event_queue(events);
 
 	return EXIT_SUCCESS;
 }
